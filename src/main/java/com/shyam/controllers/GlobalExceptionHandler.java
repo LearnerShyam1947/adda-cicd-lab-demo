52a288bf838d450a3ed99835dba151ade8f1db38
KarnamShyam1947 <karnam.shyam2004@gmail.com>
2025-02-14 09:55:31 +0530

    minor changes

--- src/main/java/com/shyam/controllers/GlobalExceptionHandler.java
+++ src/main/java/com/shyam/controllers/GlobalExceptionHandler.java
@@ -1,11 +1,12 @@
 package com.shyam.controllers;
 
+import java.util.HashMap;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.shyam.dto.ApiErrorDTO;
 import com.shyam.exceptions.StudentAlreadyExistsException;
 import com.shyam.exceptions.StudentNotFoundException;
 
@@ -13,10 +14,10 @@ import com.shyam.exceptions.StudentNotFoundException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(StudentNotFoundException.class)
-    public ResponseEntity<ApiErrorDTO> handleStudentNotFoundException(StudentNotFoundException ex) {
-        ApiErrorDTO errorResponse = new ApiErrorDTO();
-        errorResponse.setError(ex.getMessage());
-        errorResponse.setStatus(HttpStatus.NOT_FOUND.name());
+    public ResponseEntity<?> handleStudentNotFoundException(StudentNotFoundException ex) {
+        HashMap<String, Object> errorResponse = new HashMap<String, Object>();
+        errorResponse.put("error", ex.getMessage());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.name());
         
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
@@ -24,18 +25,18 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(StudentAlreadyExistsException.class)
-    public ResponseEntity<ApiErrorDTO> handleStudentAlreadyExistsException(StudentAlreadyExistsException ex) {
-        ApiErrorDTO errorResponse = new ApiErrorDTO();
-        errorResponse.setError(ex.getMessage());
-        errorResponse.setStatus(HttpStatus.CONFLICT.name());
+    public ResponseEntity<?> handleStudentAlreadyExistsException(StudentAlreadyExistsException ex) {
+        HashMap<String, Object> errorResponse = new HashMap<String, Object>();
+        errorResponse.put("error", ex.getMessage());
+        errorResponse.put("status", HttpStatus.CONFLICT.name());
         return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiErrorDTO> handleGlobalException(Exception ex) {
-        ApiErrorDTO errorResponse = new ApiErrorDTO();
-        errorResponse.setError("An unexpected error occurred");
-        errorResponse.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.name());
+    public ResponseEntity<?> handleGlobalException(Exception ex) {
+        HashMap<String, Object> errorResponse = new HashMap<String, Object>();
+        errorResponse.put("error", "An unexpected error occurred");
+        errorResponse.put("status", HttpStatus.INTERNAL_SERVER_ERROR.name());
         
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
